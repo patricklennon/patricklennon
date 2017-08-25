c5807f2da6fe55fb28cf0275d406c26213ddcbfe
patricklennon <tricklennon@gmail.com>
2017-08-24 18:37:52 -0700

    psychic game folder created

---
+++ Psychic-Game/assets/javascript/javascript.js
@@ -0,0 +1,26 @@
+
+//Global Variables
+//=============================================
+
+//var to hold wins and losses points
+//var to hold guesses left
+//var to hold guesses so far
+
+var winPoints = 0;
+var lostPoints = 0;
+var guessesLeft = 9;
+var soFarGuesses = [];
+
+
+
+
+
+
+
+
+//Functions
+//=============================================
+
+
+//Main Process
+//=============================================
